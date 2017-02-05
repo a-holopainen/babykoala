c9f0ac3104fa9c2cac18f6a0b823322a67064fd1
Anni Holopainen <anni.holopainen@kotiposti.net>
2017-02-05 15:52:48 +0100

    changed the html for the site according to tutorial

--- babykoala/views.py
+++ babykoala/views.py
@@ -1,32 +1,5 @@
-from django.http.response import HttpResponse
+
 from django.shortcuts import render
 
-
-# Create your views here.
-def index(request):
-    context = {
-        "names": {
-            "name1": "rob1",
-            "name2": "rob2",
-            "name3": "rob3",
-            "name4": "rob4",
-            "name5": "rob5",
-            "name6": "rob6"
-        },
-        "animals": [
-            {
-                "name": "bear",
-                "weight": 400
-            },
-            {
-                "name": "rabbit",
-                "weight": 15
-            },
-            {
-                "name": "whale",
-                "weight": 4000
-            }
-        ]
-    }
-
-    return render(request, 'index.html', context)
+def post_list(request):
+    return render(request, 'babykoala/post_list.html', {})
--- django_learning/urls.py
+++ django_learning/urls.py
@@ -13,13 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import include, url
 from django.contrib import admin
 
 import babykoala.views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^index/', babykoala.views.index),
-    url(r'^$', babykoala.views.index),
+    url(r'', include('babykoala.urls')),
+    # url(r'^index/', babykoala.views.index),
+    # url(r'^$', babykoala.views.index),
 ]
